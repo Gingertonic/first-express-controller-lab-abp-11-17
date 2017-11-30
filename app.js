0dcf5e3fcf7235e5bad8686daa4305b4346a28f8
Beth Schofield <bethmschofield@gmail.com>
2017-11-30 21:20:10 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express')
 var app = express()
 
+app.get("/", SiteController.Index )
 
+app.get("/about", SiteController.About)
+
+pp.get("/contact", SiteController.Contact)
 
 module.exports = app
 
